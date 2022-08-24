c80bf5a21c0094e1467c5891b2b98cc6fb42ef60
Puru Kaushik <kaushik.swapnil5@gmail.com>
2022-08-24 17:05:24 +0200

    Better attribute printing

--- QuickScript/Type.cpp
+++ QuickScript/Type.cpp
@@ -4,15 +4,29 @@
 
 void TypeInstanceDescription::Dump() const
 {
-	std::cout << std::endl << "[ ";
-	for (const auto& attr : m_Attributes)
+	if (m_Attributes.size() > 0)
 	{
-		std::cout << attr.m_Name.AsString() << " ";
+		std::cout << std::endl << "[ ";
+		for (const auto& attr : m_Attributes)
+		{
+			std::cout << attr.m_Name.AsString() << " ";
+		}
+		std::cout << "]";
 	}
-	std::cout << "]";
+	
 	std::cout << std::endl << m_Name.AsString() << "\n{\n";
 	for (const auto& member : m_Members)
 	{
+		if (member.m_Attributes.size() > 0)
+		{
+			std::cout << "[";
+			for (const auto& attr : member.m_Attributes)
+			{
+				std::cout << attr.m_Name.AsString() << " ";
+			}
+			std::cout << "]" << std::endl;
+		}
+		
 		std::cout << member.m_TypeName.AsString() << " " 
 		<< member.m_Name.AsString();
 		if (member.m_DefaultValue.IsValid())
