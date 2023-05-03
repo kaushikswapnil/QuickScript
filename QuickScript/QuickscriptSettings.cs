fff446c079b13795792ac64c7f3ec3d4e3379bb0
Puru Kaushik <kaushik.swapnil5@gmail.com>
2023-05-03 18:45:07 +0200

    Adding quickscript settings

---
+++ QuickScript/QuickscriptSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickScript
+{
+    public class QuickscriptSettings
+    {
+        public string QuickScriptsDirectory = "D:\\PersonalProjects\\QuickScript\\Tests\\Input";
+        public string OutputDirectory = "D:\\PersonalProjects\\QuickScript\\Tests\\Output";
+    }
+}
