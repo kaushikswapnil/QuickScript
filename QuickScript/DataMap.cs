1e187b0587325222251bf23e582695d34850a14b
Puru Kaushik <kaushik.swapnil5@gmail.com>
2023-05-03 19:23:57 +0200

    Adding ConstructBase<Attr/TypeDef> methods to datamap

--- QuickScript/DataMap.cs
+++ QuickScript/DataMap.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuickScript.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,48 @@ namespace QuickScript
 {
     public class DataMap
     {
-        public List<AttributeDefinition> AttributeDefinitions;
-        public List<TypeDefinition> TypeDefinitions;
+        public List<AttributeDefinition> AttributeDefinitions = new List<AttributeDefinition>();
+        public List<TypeDefinition> TypeDefinitions = new List<TypeDefinition>();
+
+        public DataMap() 
+        {
+            ConstructBaseAttributes();
+            ConstructBaseTypeDefinitions();
+        }
+        public void ConstructBaseAttributes()
+        {
+            {
+                //Filepath
+                AttributeDefinition file_path = new AttributeDefinition(new HashString("FilePath"), 1, 1);
+                AttributeDefinitions.Add(file_path); 
+            }
+        }
+
+        public void ConstructBaseTypeDefinitions()
+        {
+            {
+                //int
+                TypeDefinition integer = new TypeDefinition(new HashString("int"));
+                TypeDefinitions.Add(integer);
+            }
+
+            {
+                //char
+                TypeDefinition character = new TypeDefinition(new HashString("char"));
+                TypeDefinitions.Add(character);
+            }
+
+            {
+                //float
+                TypeDefinition floating = new TypeDefinition(new HashString("float"));
+                TypeDefinitions.Add(floating);
+            }
+
+            {
+                //string
+                TypeDefinition str = new TypeDefinition(new HashString("string"));
+                TypeDefinitions.Add(str);
+            }
+        }
     }
 }
