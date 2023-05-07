2198bc1e822a41c66db4da1020ddc274e71545f8
Puru Kaushik <kaushik.swapnil5@gmail.com>
2023-05-07 18:55:32 +0200

    Updating validate tests

--- QuickScript/Testing/Testing.cs
+++ QuickScript/Testing/Testing.cs
@@ -2,11 +2,13 @@
 using QuickScript.Utils;
 using System;
 using System.Collections.Generic;
+using System.Formats.Asn1;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace QuickScript.Testing
 {
@@ -14,21 +16,52 @@ namespace QuickScript.Testing
     { 
         protected bool Log { get; set; }
         protected uint Indent { get; set; }
-        protected void LogIfAllowed(string message)
+        protected string Name;
+        protected void LogIfAllowed(string message, uint indent)
         {
             if (Log)
             {
                 const uint IndentSize = 2;
-                string final_message = new string(' ', (int)(Indent * IndentSize)) + message;
+                string final_message = new string(' ', (int)(indent * IndentSize)) + message;
                 Console.WriteLine(message);
             }
         }
+        protected void LogIfAllowed(string message)
+        {
+            LogIfAllowed(message, Indent);
+        }
 
-        public Test(bool log= true, uint indent = 0) {  Log = log; Indent = indent; }
+        protected void LogTestBeginIfAllowed()
+        {
+            LogIfAllowed("\n-----" + Name + " Begin-----\n");
+        }
+        protected void LogTestEndIfAllowed()
+        {
+            LogIfAllowed("\n-----" + Name + " End-----\n");
+        }
+
+        protected void LogTestPassedIfAllowed(bool passed)
+        {
+            if (passed)
+            {
+                LogIfAllowed("---Test " + Name + ((passed) ? "PASSED" : "FAILED") + "---");
+            }
+        }
+
+        public Test(bool log= true, uint indent = 0, string name = "<MISSING FIELD. ENTER AT TEST CONSTRUCTOR>") {  Log = log; Indent = indent; Name = name; }
+    
+        static public DataMap GetTestDataMap(bool force_initial_basic = true, bool parse_qs_dir = true)
+        {
+            DataMap dm = DataMap.ConstructDataMap(QuickscriptSettings.DEFAULT_DATA_MAP_PATH, force_initial_basic);
+            if (parse_qs_dir)
+                dm.AssimilateTypeInstanceDescriptions(Parser.ParseDirectory(QuickscriptSettings.DEFAULT_INPUT_TEST_DIRECTORY));
+
+            return dm;
+        }
     }
     public class JsonSerializationTest<T> : Test
     {
-        public JsonSerializationTest(List<T> test_items_list, bool log = true, uint indent = 0) : base(log, indent)
+        public JsonSerializationTest(List<T> test_items_list, bool log = true, uint indent = 0) : base(log, indent, "JsonSerializationTest<" + typeof(T).FullName +">")
         {
             RunTests(test_items_list);
         }
@@ -66,7 +99,7 @@ namespace QuickScript.Testing
     }
     public class JsonSerializationTests : Test
     {
-        public JsonSerializationTests(bool log = true, uint indent = 0) : base(log, indent)
+        public JsonSerializationTests(bool log = true, uint indent = 0) : base(log, indent, typeof(JsonSerializationTests).Name)
         {
             JsonSerializationTest<HashString> test_hs =
                 new JsonSerializationTest<HashString>(new List<HashString> { new HashString("HelloWorld") }, log);
@@ -79,9 +112,63 @@ namespace QuickScript.Testing
 
     public class ValidateDataMap : Test
     {
-        public ValidateDataMap(DataMap dm, bool log = true, uint indent = 0) : base(log, indent) 
-        { 
-            //dm.
+        public ValidateDataMap(DataMap dm, bool log = true, uint indent = 0) : base(log, indent, typeof(ValidateDataMap).Name) 
+        {
+            Test(dm, log, indent);
+        }
+        private void Test(DataMap dm, bool log, uint indent)
+        {
+            LogTestBeginIfAllowed();
+
+            bool passed = true;
+
+            LogIfAllowed("Testing attributes");
+            List<AttributeDefinition> basic_attr_list = TypeInformationUtils.SupportedAttributeDefinitionList();
+            List<AttributeDefinition> attributes_in_dm = dm.AttributeDefinitions;
+            for (int attr_iter = 0; attr_iter < attributes_in_dm.Count; ++attr_iter)
+            {
+                AttributeDefinition attr_in_dm = attributes_in_dm[attr_iter];
+                for (int next_iter = attr_iter + 1; next_iter < attributes_in_dm.Count; ++next_iter)
+                {
+                    AttributeDefinition next_item = attributes_in_dm[next_iter];
+                    passed &= next_item.Name.Equals(attr_in_dm.Name) == false;
+                    Assertion.SoftAssert(next_item.Name.Equals(attr_in_dm.Name) == false,
+                        "This should have been a unique item " + attr_in_dm.Name.AsString());
+                }
+            }
+
+            foreach (AttributeDefinition basic_attr in basic_attr_list)
+            {
+                AttributeDefinition attr_in_dm_matching_name = attributes_in_dm.Find(x => x.Name == basic_attr.Name);
+                passed &= attr_in_dm_matching_name != null;
+                Assertion.SoftAssert(attr_in_dm_matching_name != null, "We should have one attribute with a matching name for " + basic_attr.Name.AsString());
+            }
+
+            LogIfAllowed("Testing type defs");
+            List<TypeDefinition> basic_types_list = TypeInformationUtils.SupportedBasicTypeDefinitionsList();
+            List<TypeDefinition> types_in_dm = dm.TypeDefinitions;
+            for (int type_iter = 0; type_iter < attributes_in_dm.Count; ++type_iter)
+            {
+                TypeDefinition type_in_dm = types_in_dm[type_iter];
+                for (int next_iter = type_iter + 1; next_iter < types_in_dm.Count; ++next_iter)
+                {
+                    TypeDefinition next_item = types_in_dm[next_iter];
+                    passed &= next_item.Name.Equals(type_in_dm.Name) == false;
+                    Assertion.SoftAssert(next_item.Name.Equals(type_in_dm.Name) == false,
+                        "This should have been a unique item " + type_in_dm.Name.AsString());
+                }
+            }
+
+            foreach (TypeDefinition basic_type in basic_types_list)
+            {
+                TypeDefinition type_in_dm_matching_name = types_in_dm.Find(x => x.Name == basic_type.Name);
+                passed &= type_in_dm_matching_name != null;
+                Assertion.SoftAssert(type_in_dm_matching_name != null, "We should have one type definition with a matching name for " + basic_type.Name.AsString());
+            }
+
+            LogTestPassedIfAllowed(passed);
+
+            LogTestEndIfAllowed();
         }
     }
 
@@ -90,20 +177,17 @@ namespace QuickScript.Testing
         public CreateAndSerializeTestDataMap(bool initial_basic = true, bool log = true, uint indent = 0) : base(log, indent)
         {
             LogIfAllowed("-----CreateAndSerializeTestDataMap Test : Beginning-----\n");
-            QuickscriptSettings settings = new QuickscriptSettings();
 
-            DataMap dm = DataMap.ConstructDataMap(settings.DataMapPath, initial_basic);
-            dm.AssimilateTypeInstanceDescriptions(Parser.ParseDirectory(settings.QuickScriptsDirectory));
-
+            DataMap dm = GetTestDataMap(initial_basic, true);
             LogIfAllowed("Constructed datamap as " + dm);
 
             LogIfAllowed("Attempting dm serialization");
             JsonSerializationTest<DataMap> test_dm = new JsonSerializationTest<DataMap>(dm, log, ++indent);
             LogIfAllowed("Saving to dm");
-            dm.SaveToDisk(settings.DataMapPath);
+            dm.SaveToDisk(QuickscriptSettings.DEFAULT_DATA_MAP_PATH);
 
-            LogIfAllowed("Attempting reconstruction from disk save path " + settings.DataMapPath);
-            DataMap new_dm = DataMap.ConstructDataMap(settings.DataMapPath);
+            LogIfAllowed("Attempting reconstruction from disk save path " + QuickscriptSettings.DEFAULT_DATA_MAP_PATH);
+            DataMap new_dm = DataMap.ConstructDataMap(QuickscriptSettings.DEFAULT_DATA_MAP_PATH);
 
             LogIfAllowed("Constructed new dm as " + new_dm);
             bool passed = dm.Equals(new_dm);
@@ -120,6 +204,7 @@ namespace QuickScript.Testing
         { 
             JsonSerializationTests test_json = new JsonSerializationTests(log, indent);
             CreateAndSerializeTestDataMap test_dm_create = new CreateAndSerializeTestDataMap(true, log, indent);
+            ValidateDataMap test_dm_val = new ValidateDataMap(GetTestDataMap(), log, indent);
         }
     }
 }
