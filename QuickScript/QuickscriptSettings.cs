bda4a381de4df7de64ac52f243c09a6d843f683b
Puru Kaushik <kaushik.swapnil5@gmail.com>
2023-05-07 18:54:51 +0200

    Adding default quickscript settings

--- QuickScript/QuickscriptSettings.cs
+++ QuickScript/QuickscriptSettings.cs
@@ -8,8 +8,11 @@ namespace QuickScript
 {
     public class QuickscriptSettings
     {
-        public string QuickScriptsDirectory = "D:\\PersonalProjects\\QuickScript\\Tests\\Input";
-        public string OutputDirectory = "D:\\PersonalProjects\\QuickScript\\Tests\\Output";
-        public string DataMapPath = "D:\\PersonalProjects\\QuickScript\\Tests\\QSDataMap.qsdm";
+        static readonly public string DEFAULT_INPUT_TEST_DIRECTORY = "D:\\PersonalProjects\\QuickScript\\Tests\\Input";
+        static readonly public string DEFAULT_OUTPUT_TEST_DIRECTORY = "D:\\PersonalProjects\\QuickScript\\Tests\\Output";
+        static readonly public string DEFAULT_DATA_MAP_PATH = "D:\\PersonalProjects\\QuickScript\\Tests\\QSDataMap.qsdm";
+        public string QuickScriptsDirectory = DEFAULT_INPUT_TEST_DIRECTORY;
+        public string OutputDirectory = DEFAULT_OUTPUT_TEST_DIRECTORY;
+        public string DataMapPath = DEFAULT_DATA_MAP_PATH;
     }
 }
