7a42675ad4176736c0f995dcb66e339e00131222
Puru Kaushik <kaushik.swapnil5@gmail.com>
2023-05-08 21:55:00 +0200

    Fixing path ends for settings

--- QuickScript/QuickscriptSettings.cs
+++ QuickScript/QuickscriptSettings.cs
@@ -8,8 +8,8 @@ namespace QuickScript
 {
     public class QuickscriptSettings
     {
-        static readonly public string DEFAULT_INPUT_TEST_DIRECTORY = "D:\\PersonalProjects\\QuickScript\\Tests\\Input";
-        static readonly public string DEFAULT_OUTPUT_TEST_DIRECTORY = "D:\\PersonalProjects\\QuickScript\\Tests\\Output";
+        static readonly public string DEFAULT_INPUT_TEST_DIRECTORY = "D:\\PersonalProjects\\QuickScript\\Tests\\Input\\";
+        static readonly public string DEFAULT_OUTPUT_TEST_DIRECTORY = "D:\\PersonalProjects\\QuickScript\\Tests\\Output\\";
         static readonly public string DEFAULT_DATA_MAP_PATH = "D:\\PersonalProjects\\QuickScript\\Tests\\QSDataMap.qsdm";
         public string QuickScriptsDirectory = DEFAULT_INPUT_TEST_DIRECTORY;
         public string OutputDirectory = DEFAULT_OUTPUT_TEST_DIRECTORY;
